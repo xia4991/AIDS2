7c717b35bc6af0cbee1cd4d4e5dc51aea3d5f979
SongaMonga <32817093+SongaMonga@users.noreply.github.com>
2017-11-27 18:36:56 +0000

    falta tuple_list[6]

--- convert.py
+++ convert.py
@@ -22,31 +22,58 @@ def test_sentence(sentence):
     }.get(sentence, 0)
 
 
-def convert(sentence):
+def convert(sentence, clause_list):
 	sentence_list = []
 	if test_sentence(sentence[0]) == 0:						#veifica se o sentence e atom
-		return sentence
+		return(sentence[0])
+
 	elif test_sentence(sentence[0]) == 1:
-		if test_sentence(sentence[1][0]) == 0:					#verifica se o sentence é not atom
-			return sentence
-		elif sentence[1][0] == 'and':
-			new_sentence = ('not', sentence[1][1])
-			sentence_list.extend(convert(sentence[1]))
-			return sentence_list
+		if test_sentence(sentence[1][0]) == 0:
+			return(sentence)
+
+		elif test_sentence(sentence[1][0]) == 1:
+			clause_list = convert(sentence[1][1], clause_list)
+
+		elif test_sentence(sentence[1][0]) == 2:
+			deMorgan = ('or', ('not', sentence[1][1]), ('not', sentence[1][2]))
+			clause_list = convert(deMorgan, clause_list)
+
+		elif test_sentence(sentence[1][0]) == 3:
+			deMorgan = ('and', ('not', sentence[1][1]), ('not', sentence[1][2]))
+			clause_list = convert(deMorgan, clause_list)
+
+		elif test_sentence(sentence[1][0]) == 4:
+			n_conditional = ('and', sentence[1][1], ('not', sentence[1][2]))
+			clause_list = convert(n_conditional, clause_list)
+
+		elif test_sentence(sentence[1][0]) == 5:
+			n_biconditional = ('or', ('and', sentence[1][1], ('not', sentence[1][2])), ('and', sentence[1][2], ('not', sentence[1][1])))
+			clause_list = convert(n_biconditional, clause_list)
+
 	elif test_sentence(sentence[0]) == 2:
-		return
+		clause_list.append(convert(sentence[1], clause_list))
+		clause_list.append(convert(sentence[2], clause_list))
+
 	elif test_sentence(sentence[0]) == 3:
-		return
+		sentence_list.append(convert(sentence[1], clause_list))
+		sentence_list.append(convert(sentence[2], clause_list))
+		return(sentence_list)
+
 	elif test_sentence(sentence[0]) == 4:
-		return
+		conditional = ('or', ('not', sentence[1]), sentence[2])
+		clause_list = convert(conditional, clause_list)
+
 	elif test_sentence(sentence[0]) == 5:
-		return
+		biconditional = ('and', ('or', ('not', sentence[1]), sentence[2]), ('or', ('not', sentence[2]), sentence[1]))
+		clause_list = convert(biconditional, clause_list)
+
+	return clause_list
 
 
 
 def main():
-	print (sys.argv[1])
-	read_document(sys.argv[1])
+	#print (sys.argv[1])
+	#read_document(sys.argv[1])
 
 	tuple_list = []
 	negation = ('not', 'A')
@@ -63,13 +90,14 @@ def main():
 	tuple_list.append(('not', ('and', 'A' , 'B')))
 	tuple_list.append(('or', ('and', 'A' , 'B'), ('and', 'C' , 'B')))
 	tuple_list.append(('not',('or', ('and', 'A' , 'B'), ('and', 'C' , 'B'))))
-	
+
 	#print (tuple_list)
 
-	print (convert(tuple_list[5]))
+	clause_list = convert(tuple_list[6], [])
+	#set(clause_list)
 
-
-
+	for c in clause_list:
+		print (c)
 
 
 if __name__ == "__main__":
